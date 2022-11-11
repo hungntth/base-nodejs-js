9a36a36eef325690d3cd8f2c2fa678e6a3b6be3a
Nguyen Thai Hung <hungnt@techasians.com>
2022-11-11 11:43:02 +0700

    first code

---
+++ src/config/postgresql.js
@@ -0,0 +1,23 @@
+module.exports = {
+    localhost: {
+        host: "127.0.0.1",
+        port: 5432,
+        username: "postgres",
+        password: "admin",
+        dbName: "base_db"
+    },
+    development: {
+        host: "127.0.0.1",
+        port: 5432,
+        username: "postgres",
+        password: "admin",
+        dbName: "base_db"
+    },
+    production: {
+        host: "127.0.0.1",
+        port: 5432,
+        username: "postgres",
+        password: "admin",
+        dbName: "base_db"
+    }
+}
